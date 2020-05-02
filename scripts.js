43282ba65677c57e24e2758d989405335e1ce38f
BgsI2011 <64489630+BgsI2011@users.noreply.github.com>
2020-05-02 11:15:52 +0200

    add firs part

--- scripts.js
+++ scripts.js
@@ -1,1 +1,14 @@
-console.log('Script file is loades.')
+console.log('Script file is loaded.');
+
+// Single line comment
+/* Multiple
+lines
+of comments
+*/
+//Firstpart:
+// 1+1
+// 2
+// 3+5
+// 8
+// 55+66+5
+// 126
